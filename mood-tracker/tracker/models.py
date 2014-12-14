949657148a17bcb2a6fbdd92b1468a110f60c1f8
SamCook42 <gitsam@samlr.com>
2014-12-14 16:07:41 +0000

    hash out tracker models

--- mood-tracker/tracker/models.py
+++ mood-tracker/tracker/models.py
@@ -1,6 +1,29 @@
-from django.db import models
+"""
+Really basic set of log entries. Event Type is a general set of types.
+"""
 
-# Create your models here.
 
-class Tracker(models.Model):
-    pass
+from django.db import models as django_models
+from model_utils import models as model_utils
+
+class TrackerLogEntry(model_utils.TimeStampedModel,
+                      model_utils.TimeFramedModel):
+    """
+    A log entry, has a start/end time and a JSON object which stores the
+    data of event. Each entry has an 'event type' which is the primary
+    classification of the log.
+
+    Entries are timestamped with modified and created datetimes.
+    """
+    # TimeFramedModel gives start/end -- for logging when the event happened
+    # TimeStampedModel gives created/modified -- mostly for conflict resolution
+    event_type = django_models.ForeignKey('EventType', db_index=True)
+    event_data = django_models.TextField()
+
+
+class EventType(django_models.Model):
+    """
+    What sort of event is being logged. Each has name and slug.
+    """
+    name = django_models.CharField(max_length=50)
+    slug = django_models.SlugField()
