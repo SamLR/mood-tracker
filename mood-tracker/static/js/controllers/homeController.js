d49aceb74cc10b3f0a0da4fea637a88c02d54999
SamCook42 <gitsam@samlr.com>
2014-12-20 18:30:05 +0000

    Test post/put

--- mood-tracker/static/js/controllers/homeController.js
+++ mood-tracker/static/js/controllers/homeController.js
@@ -1,7 +1,19 @@
+/* global angular, moment */
+
 angular.module('moodTracker').controller('homeController', [
-    '$scope',
-    function ($scope) {
+    '$scope', '$http',
+    function ($scope, $http) {
         'use strict';
-        $scope.hello = 'hello world!';
+
+        $scope.sendEvent = function() {
+            $http.put('api/logs/20', {
+                user: 1,
+                event_type: 1,
+                start: moment([2010, 1, 1, 6, 6, 6]),
+                end:   moment([2010, 2, 2, 10, 10, 10]),
+                data: JSON.stringify({something: 'completely different (CHEESE!)'}),
+                tags: [1,2]
+            });
+        };
     }
 ]);
