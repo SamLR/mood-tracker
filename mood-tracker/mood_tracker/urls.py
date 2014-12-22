74690524653ab9d6d7c66d2b8735609f73effa96
SamCook42 <gitsam@samlr.com>
2014-12-22 13:05:21 +0000

    re-route unfound stuff to base

--- mood-tracker/mood_tracker/urls.py
+++ mood-tracker/mood_tracker/urls.py
@@ -12,6 +12,7 @@ urlpatterns = patterns('',
     url(r'^$', BaseView.as_view(), name='home'),
     url(r'^api/', include(tracker.api_patterns)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', BaseView.as_view()), # catch all
 )
 
 
